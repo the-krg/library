c60a7ed2c0c913da5f41e6c8b85c6315ec4c0103
Enzo Haguiwara <haguiwara68@gmail.com>
2021-08-03 13:20:16 -0300

    fix book factory

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :book do
-    name { Faker::Book.name }
+    name { Faker::Book.title }
     description { Faker::Quote.matz }
     book_genre { create(:book_genre) }
+    available { true }
   end
 end
