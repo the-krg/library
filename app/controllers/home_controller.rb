1d0828059ac06f1c58d7f089ccf24dd19c7f6199
Enzo Haguiwara <haguiwara68@gmail.com>
2021-08-04 03:34:15 -0300

    remove n+1 from home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    render locals: { rentals: Rental.borrowed.order(borrow_date: :asc) }
+    render locals: { 
+      rentals: Rental.includes(:user, :book).borrowed.order(borrow_date: :asc) 
+    }
   end
 end
